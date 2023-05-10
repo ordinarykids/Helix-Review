6391b493e38427f8e38b3d8a79714264d5f6580a
Conrad Seto <conrad@alephsf.com>
2023-05-10 11:07:46 -0700

    feat: Port Sanity config and environment files

---
+++ environment.ts
@@ -0,0 +1,4 @@
+export const sanityProjectId = process.env.SANITY_STUDIO_PROJECT_ID
+export const sanityDataset = process.env.SANITY_STUDIO_DATASET
+export const sanityApiToken = process.env.SANITY_STUDIO_API_TOKEN
+export const sanityCacheToken = process.env.SANITY_STUDIO_CACHE_TOKEN
