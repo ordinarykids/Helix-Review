12ec3e85e7ddc87c98957b90320572a014b3121b
Amanda Tusing <amanda@alephsf.com>
2023-05-11 15:05:17 -0700

    feat: Move lib and schema folders into sanity folder

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
-import { schemaTypes } from './schemas'
+import { schemaTypes } from './sanity/schemas'
 import { sanityDataset, sanityProjectId } from './environment'
 
 export default defineConfig({
