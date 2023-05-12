5f2743e28970445d69adb11e5ae4aecb76e4a596
Amanda Tusing <amanda@alephsf.com>
2023-05-12 14:10:25 -0700

    chore: Fix Sanity codegen include path

--- sanity-codegen.config.ts
+++ sanity-codegen.config.ts
@@ -2,7 +2,7 @@ import { SanityCodegenConfig } from '@sanity-codegen/cli'
 
 const config: SanityCodegenConfig = {
   output: 'types/sanity-codegen/sanity-codegen.d.ts',
-  include: ['./app/(frontend)/**/*.{js,jsx,ts,tsx}'],
+  include: ['./app/\\(frontend\\)/**/*.{js,jsx,ts,tsx}'],
 }
 
 export default config
