eb98753125c3f5b111ce6ade3dcebe7cf1a1edcd
Conrad Seto <conrad@alephsf.com>
2023-05-23 15:42:45 -0700

    feat: Build Hero

--- app/(frontend)/components/PageHero/PageHero.tsx
+++ app/(frontend)/components/PageHero/PageHero.tsx
@@ -1,36 +1,61 @@
-'use client'
-
-import { useState, useEffect } from 'react'
 import classNames from 'classnames'
+import Image, { StaticImageData } from 'next/image'
+// import CardWithImage from '../CardWithImage/CardWithImage'
 import styles from './PageHero.module.scss'
 
+// interface CardWithImageProps {
+//   title: string,
+//   src: StaticImageData,
+//   width: number,
+//   height: number,
+//   alt: string,
+// }
+
 interface PageHeroProps {
-  text: string
+  header: string,
+  subheader: string,
+  src: StaticImageData,
+  width: number,
+  height: number,
+  alt: string,
+  animation: string,
+  // cardWithImageProps: CardWithImageProps,
 }
 
-export default function PageHero({ text }: PageHeroProps) {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    document.title = `${text} - ${count} times`
-  }, [count, text])
-
-  const containerClasses = classNames(styles.container, styles.someOtherClass)
+export default function PageHero({
+  header, subheader, src, width, height, alt, animation,
+}: PageHeroProps) {
+  const containerClasses = classNames(styles.container)
 
   return (
     <div className={containerClasses}>
-      <h1>{text}</h1>
-      <button
-        type='button'
-        onClick={() => setCount(count + 1)}
-      >
-        Click me
-      </button>
-      <p>
-        You clicked
-        {count}
-        times
-      </p>
+      <div className={classNames(styles.hero)}>
+        <div className={classNames(styles.heroLeft)}>
+          <h2 className={classNames(styles.heroHeader)}>{header}</h2>
+          <p className={classNames(styles.heroSubheader)}>{subheader}</p>
+          {/* StyledLink component */}
+        </div>
+        <div className={classNames(styles.heroRight)}>
+          {animation
+            ? (
+              <h2>FPO animation</h2>
+            )
+            : (
+              <Image
+                className={classNames(styles.image)}
+                src={src}
+                width={width}
+                height={height}
+                alt={alt}
+              // fill
+              // style={{ objectFit: 'contain' }}
+              />
+            )}
+        </div>
+      </div>
+      <div className={classNames(styles.subsection)}>
+        {/* <CardWithImage title='foo' src={[]} height={100} width={100} /> */}
+      </div>
     </div>
   )
 }
