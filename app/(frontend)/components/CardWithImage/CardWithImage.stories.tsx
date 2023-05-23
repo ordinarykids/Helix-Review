92ae22e1a3fdb07f1b182668d26dc31daaaf1c6a
Conrad Seto <conrad@alephsf.com>
2023-05-22 21:36:44 -0700

    chore: Remove decorator inline style

--- app/(frontend)/components/CardWithImage/CardWithImage.stories.tsx
+++ app/(frontend)/components/CardWithImage/CardWithImage.stories.tsx
@@ -14,9 +14,6 @@ const containerStyle = { // move to global?
   columnGap: '24px',
   gridTemplateColumns: 'repeat(12, 1fr)',
 }
-const wrapStyle = {
-  border: '1px dashed magenta',
-}
 
 const meta: Meta<typeof CardWithImage> = {
   title: 'Components/CardWithImage',
@@ -24,7 +21,7 @@ const meta: Meta<typeof CardWithImage> = {
   //   tags: ['autodocs'], // Uncomment if you aren't using an MDX file for docs here
   decorators: [
     (Story) => (
-      <div style={wrapStyle}>
+      <div>
         <Story />
       </div>
     ),
