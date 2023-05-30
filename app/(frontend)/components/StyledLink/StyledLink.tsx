d08858ae42dd611e30489103450ea71ffbbc49ce
Conrad Seto <conrad@alephsf.com>
2023-05-30 01:42:59 -0700

    feat: Export StyledLinkProps interface

--- app/(frontend)/components/StyledLink/StyledLink.tsx
+++ app/(frontend)/components/StyledLink/StyledLink.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames'
 import ArrowRight from '../svgs/ArrowRight'
 import styles from './StyledLink.module.scss'
 
-interface StyledLinkProps {
+export interface StyledLinkProps {
   text: string,
   link: string,
   style: 'primary' | 'secondary' | 'carat',
