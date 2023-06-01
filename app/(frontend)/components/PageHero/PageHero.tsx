0fa1b8bf96e69e1a684c408c30d3cbd349154606
Conrad Seto <conrad@alephsf.com>
2023-06-01 14:09:11 -0700

    chore: Remove hero wrapper div

--- app/(frontend)/components/PageHero/PageHero.tsx
+++ app/(frontend)/components/PageHero/PageHero.tsx
@@ -33,36 +33,34 @@ export default function PageHero({
 
   return (
     <div className={containerClasses}>
-      <div className={classNames(styles.hero)}>
-        <div className={classNames(styles.heroLeft)}>
-          <h2 className={classNames(styles.heroHeader)}>{header}</h2>
-          <p className={classNames(styles.heroSubheader)}>{subheader}</p>
-          <div className={classNames(styles.link)}>
-            <StyledLink
-              text={buttonText}
-              link={buttonLink}
-            />
-          </div>
+      <div className={classNames(styles.heroLeft)}>
+        <h2 className={classNames(styles.heroHeader)}>{header}</h2>
+        <p className={classNames(styles.heroSubheader)}>{subheader}</p>
+        <div className={classNames(styles.link)}>
+          <StyledLink
+            text={buttonText}
+            link={buttonLink}
+          />
         </div>
-        <div className={classNames(styles.heroRight)}>
-          {media
-            ? (
-              <div className={classNames(styles.heroAnimation)}>
-                <h2>FPO animation</h2>
-              </div>
-            )
-            : (
-              <div className={classNames(styles.heroImage)}>
-                <Image
-                  className={classNames(styles.image)}
-                  src={src}
-                  width={width}
-                  height={height}
-                  alt={alt}
-                />
-              </div>
-            )}
-        </div>
+      </div>
+      <div className={classNames(styles.heroRight)}>
+        {media
+          ? (
+            <div className={classNames(styles.heroAnimation)}>
+              <h2>FPO animation</h2>
+            </div>
+          )
+          : (
+            <div className={classNames(styles.heroImage)}>
+              <Image
+                className={classNames(styles.image)}
+                src={src}
+                width={width}
+                height={height}
+                alt={alt}
+              />
+            </div>
+          )}
       </div>
       <div className={classNames(styles.subsection)}>
         {/* <CardWithImage title='foo' src={[]} height={100} width={100} /> */}
