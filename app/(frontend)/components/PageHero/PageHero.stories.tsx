ac4a17e7f2f33e3f16b1754c0aac5dbeaa3f0807
Conrad Seto <conrad@alephsf.com>
2023-06-07 15:10:51 -0700

    feat: Update story args to match pageHero type; copy subheader object from GROQ to match PortableTextBlock type

--- app/(frontend)/components/PageHero/PageHero.stories.tsx
+++ app/(frontend)/components/PageHero/PageHero.stories.tsx
@@ -5,7 +5,7 @@ import PageHero from '.'
 const meta: Meta<typeof PageHero> = {
   title: 'Components/PageHero',
   component: PageHero,
-  //   tags: ['autodocs'], // Uncomment if you aren't using an MDX file for docs here
+  tags: ['autodocs'], // Uncomment if you aren't using an MDX file for docs here
 }
 
 export default meta
@@ -14,13 +14,36 @@ type Story = StoryObj<typeof PageHero>;
 
 export const HomePage: Story = {
   args: {
-    header: 'Weaving Genomics into the Fabric of Healthcare',
-    subheader: 'Helix transforms health organizations to leverage the power of genomics across their enterprise; creating strategic value and fueling future growth.',
-    src: heroFPOImage,
-    width: 434,
-    height: 427,
-    alt: 'Hero FPO image',
-    buttonText: 'Contact Us',
-    buttonLink: 'https://helix.com',
+    pageHero: {
+      header: 'Weaving Genomics into the Fabric of Healthcare',
+      subheader: {
+        children: [
+          {
+            _key: 'c5080bab2071',
+            _type: 'span',
+            marks: [],
+            text: 'Helix transforms health organizations to leverage the power of genomics across their enterprise; creating strategic value and fueling future growth.',
+          },
+        ],
+        _type: 'block',
+        markDefs: [],
+        style: 'normal',
+        _key: 'c5080bab2071',
+      },
+      image: {
+        src: heroFPOImage,
+        width: 434,
+        height: 427,
+        alt: 'Hero FPO image',
+      },
+      button: {
+        text: 'Contact Us',
+        link: 'https://helix.com',
+        linkStyle: 'button',
+        size: 'large',
+        align: 'left',
+        theme: 'dark',
+      },
+    },
   },
 }
