6393e32c9fef7c2888e8ce407dd6e6fc1f22c87a
Conrad Seto <conrad@alephsf.com>
2023-06-07 11:46:21 -0700

    feat: Add codegen required to required Sanity fields

--- sanity/schemas/objects/styledLink.js
+++ sanity/schemas/objects/styledLink.js
@@ -14,12 +14,14 @@ export default {
       type: 'string',
       title: 'Text',
       validation: (Rule) => Rule.required(),
+      codegen: { required: true },
     }),
     defineField({
       name: 'link',
       type: 'url',
       title: 'Link',
       validation: (Rule) => Rule.required(),
+      codegen: { required: true },
     }),
     defineField({
       name: 'style',
