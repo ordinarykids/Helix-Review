be0f468ab3eee1e5faebed8828c53f8bdce91c12
Conrad Seto <conrad@alephsf.com>
2023-06-07 12:04:54 -0700

    feat: Refactor GROQ query, create PageHeroType manually

--- app/(frontend)/lib/sanity/fetch/fetchPageByPath.ts
+++ app/(frontend)/lib/sanity/fetch/fetchPageByPath.ts
@@ -1,12 +1,45 @@
 import { codegen, groq } from '@sanity-codegen/client'
+import { ImageProps } from 'next/image'
+import type { PortableTextBlock } from '@portabletext/types'
+import type { StyledLinkProps } from '@/app/(frontend)/components/StyledLink/StyledLink'
 import { sanityFetch } from '../sanityClient'
+
+export type PageHeroType = {
+  title?: string | null,
+  pageHero?: {
+    header?: string | null,
+    subheader?: PortableTextBlock | null,
+    media?: string | null,
+    button: StyledLinkProps | null,
+    image: ImageProps | null,
+  } | null,
+}
 
 const fetchPageByPath = async (pagePath: string) => {
   const query = codegen(
     'PageByPath',
     groq`*[_type == "page" && slug.current == $pagePath][0]{
       title,
-      hero,
+      'pageHero': hero {
+        header,
+        subheader,
+        media,
+        'button': {
+          'text': styledLink.text,
+          'link': styledLink.link,
+          'style': styledLink.style,
+          'align': styledLink.align,
+          'type': styledLink._type,
+            },
+        'image': {
+          'src': image.asset->url,
+          'alt': image.asset->metadata.altText,
+          'height': image.asset->metadata.dimensions.height,
+          'width': image.asset->metadata.dimensions.width,
+          'aspectRatio': image.asset->metadata.dimensions.aspectRatio,
+          'blurHash': metadata.blurHash,
+        },
+      },
     }`,
   )
   const res = await sanityFetch<Sanity.Default.Query.PageByPath>(query, { pagePath })
