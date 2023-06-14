3fa26d10812610b9bda9e9fcdb046f67fa95c923
Conrad Seto <conrad@alephsf.com>
2023-06-14 01:10:24 -0700

    feat: Add more props to ButtonProps, set defaults in component

--- app/(frontend)/components/Button/Button.tsx
+++ app/(frontend)/components/Button/Button.tsx
@@ -6,14 +6,14 @@ interface ButtonProps {
   text: string,
   link: string,
   className?: string,
-  buttonStyle: 'solid' | 'outline',
-  size: 'large' | 'small',
-  theme: 'dark' | 'light',
-  align: 'center' | 'left',
+  buttonStyle?: 'solid' | 'outline',
+  size?: 'large' | 'small',
+  theme?: 'dark' | 'light',
+  align?: 'center' | 'left',
 }
 
 export default function Button({
-  text, link, className, buttonStyle, size, align, theme,
+  text, link, className, buttonStyle = 'solid', size = 'large', align = 'left', theme = 'dark',
 }: ButtonProps) {
   const linkClasses = cx(styles.Button, styles[buttonStyle], styles[size], styles[align], styles[theme])
 
