a1455e8afb22de92d6a03fb0f047981f6c7e4b48
Conrad Seto <conrad@alephsf.com>
2023-06-14 14:46:58 -0700

    feat: Convert StyledLink schema to caret only

--- sanity/schemas/objects/styledLink.js
+++ sanity/schemas/objects/styledLink.js
@@ -5,14 +5,8 @@ export default {
   type: 'object',
   title: 'Link',
   initialValue: {
-    linkStyle: 'button',
-    theme: 'dark',
-    size: 'large',
     align: 'left',
   },
-  fieldsets: [
-    { name: 'linkSettings', title: 'Settings' },
-  ],
   fields: [
     defineField({
       name: 'text',
@@ -29,55 +23,15 @@ export default {
       codegen: { required: true },
     }),
     defineField({
-      name: 'linkStyle',
-      type: 'string',
-      title: 'Style',
-      fieldset: 'linkSettings',
-      options: {
-        list: [
-          { title: 'Button', value: 'button' },
-          { title: 'Carat', value: 'carat' },
-        ],
-      },
-    }),
-    defineField({
-      name: 'theme',
-      type: 'string',
-      title: 'Theme',
-      fieldset: 'linkSettings',
-      options: {
-        list: [
-          { title: 'Dark', value: 'dark' },
-          { title: 'Light', value: 'light' },
-        ],
-      },
-      hidden: ({ parent }) => parent?.linkStyle === 'carat',
-    }),
-    defineField({
-      name: 'size',
-      type: 'string',
-      title: 'Size',
-      fieldset: 'linkSettings',
-      options: {
-        list: [
-          { title: 'Large', value: 'large' },
-          { title: 'Small', value: 'small' },
-        ],
-      },
-      hidden: ({ parent }) => parent?.linkStyle === 'carat',
-    }),
-    defineField({
       name: 'align',
       type: 'string',
       title: 'Align',
-      fieldset: 'linkSettings',
       options: {
         list: [
           { title: 'Left', value: 'left' },
           { title: 'Center', value: 'center' },
         ],
       },
-      hidden: ({ parent }) => parent?.size === 'small',
     }),
   ],
 }
