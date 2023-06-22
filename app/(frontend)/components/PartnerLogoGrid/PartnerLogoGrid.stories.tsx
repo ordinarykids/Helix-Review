de9e2aa507e9307b7ce867547255ae59ce0540a9
Conrad Seto <conrad@alephsf.com>
2023-06-22 12:18:07 -0700

    feat: Add placeholders to story

--- app/(frontend)/components/PartnerLogoGrid/PartnerLogoGrid.stories.tsx
+++ app/(frontend)/components/PartnerLogoGrid/PartnerLogoGrid.stories.tsx
@@ -16,7 +16,33 @@ type Story = StoryObj<typeof PartnerLogoGrid>;
 
 export const Default: Story = {
   args: {
-    header: 'Hello World!',
-    subheader: 'what',
+    header: 'Some of Our Partners',
+    subheader: 'Driving collaboration and partnership across the industry:',
+    logos: [
+      {
+        image: {
+          url: 'https://cdn.sanity.io/images/g5irbagy/staging/e97e0ebbb51c68f7a03433b85fba83c021d12482-748x656.png',
+          altText: '',
+          title: '',
+          height: 656,
+          width: 748,
+          aspectRatio: 1.1402439024390243,
+          blurHash: 'eoPi@mo#t7xu_4yEs+RjWXs.-=M|t7t7Mxn#WCofV@Rjx^t6RjR*Ri',
+        },
+        external: 'foo',
+      },
+      {
+        image: {
+          url: 'https://cdn.sanity.io/images/g5irbagy/staging/e97e0ebbb51c68f7a03433b85fba83c021d12482-748x656.png',
+          altText: '',
+          title: '',
+          height: 656,
+          width: 748,
+          aspectRatio: 1.1402439024390243,
+          blurHash: 'eoPi@mo#t7xu_4yEs+RjWXs.-=M|t7t7Mxn#WCofV@Rjx^t6RjR*Ri',
+        },
+        fileUrl: '#',
+      },
+    ],
   },
 }
