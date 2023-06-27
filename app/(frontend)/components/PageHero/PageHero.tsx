83494bce9f34b9295f95e2729c7d0ec7cc883eb6
Amanda Tusing <amanda@alephsf.com>
2023-06-26 17:04:29 -0700

    feat: Add simple page hero option styling

--- app/(frontend)/components/PageHero/PageHero.tsx
+++ app/(frontend)/components/PageHero/PageHero.tsx
@@ -15,43 +15,53 @@ export type PageHeroType = {
     buttonText?: string | null,
     buttonLink?: LinkField,
     image: ImageField,
+    simpleHeader?: boolean | null
   }
 
 export default function PageHero({
-  eyebrow, header, subheader, buttonText, buttonLink, image,
+  eyebrow, header, subheader, buttonText, buttonLink, image, simpleHeader = false,
 }: PageHeroType) {
-  console.log(header)
   return (
-    <div className={cx(styles.wrap)}>
-      <div className={cx(styles.container)}>
-        <div className={cx(styles.imageWrap)}>
-          {image && (
-            <Image
-              className={cx(styles.image)}
-              src={image.url}
-              width={image.width}
-              height={image.height}
-              alt={image.altText ?? ''}
-              title={image.title ?? undefined}
-            />
-          )}
-        </div>
-        <div className={cx(styles.textWrap)}>
-          {eyebrow && <p className={styles.eyebrow}>{eyebrow}</p>}
-          {header && <h1 className={cx(styles.header)}>{header}</h1>}
-          {subheader && (
-            <div className={cx(styles.subheader)}>
-              <PortableText value={subheader} />
+    <div className={cx(styles.wrap, { [styles.wrap__simple]: simpleHeader })}>
+      <div className={styles.container}>
+        {simpleHeader ? (
+          header && (
+            <div className={styles.simpleHeader}>
+              <h1 className={cx(styles.header, { [styles.header__simple]: simpleHeader })}>{header}</h1>
             </div>
-          )}
-          {(buttonText && buttonLink) && (
-            <Button
-              text={buttonText}
-              link={`${buttonLink.link || buttonLink.externalUrl}`}
-              size='small'
-            />
-          )}
-        </div>
+          )
+        ) : (
+          <>
+            <div className={cx(styles.imageWrap)}>
+              {image && (
+                <Image
+                  className={styles.image}
+                  src={image.url}
+                  width={image.width}
+                  height={image.height}
+                  alt={image.altText ?? ''}
+                  title={image.title ?? undefined}
+                />
+              )}
+            </div>
+            <div className={cx(styles.textWrap)}>
+              {eyebrow && <p className={styles.eyebrow}>{eyebrow}</p>}
+              {header && <h1 className={styles.header}>{header}</h1>}
+              {subheader && (
+                <div className={cx(styles.subheader)}>
+                  <PortableText value={subheader} />
+                </div>
+              )}
+              {(buttonText && buttonLink) && (
+                <Button
+                  text={buttonText}
+                  link={`${buttonLink.link || buttonLink.externalUrl}`}
+                  size='small'
+                />
+              )}
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
