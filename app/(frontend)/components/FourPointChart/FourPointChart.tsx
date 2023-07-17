5cab226226416efed55cadb90b02758d519c47d0
Amanda Tusing <amanda@alephsf.com>
2023-07-17 11:48:15 -0700

    feat: Add bg hover svgs to FourPointChart
    And move FourPointChart-specific svgs into FourPointChart directory

--- app/(frontend)/components/FourPointChart/FourPointChart.tsx
+++ app/(frontend)/components/FourPointChart/FourPointChart.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import DashedLineSmallHorizontal from '../svgs/DashedLineSmallHorizontal'
-import DashedLineSmallVertical from '../svgs/DashedLineSmallVertical'
+import DashedLineSmallHorizontal from './svgs/DashedLineSmallHorizontal'
+import DashedLineSmallVertical from './svgs/DashedLineSmallVertical'
+import Circle from './svgs/Circle'
+import Diamond from './svgs/Diamond'
+import Hexagon from './svgs/Hexagon'
+import Triangle from './svgs/Triangle'
 import styles from './FourPointChart.module.scss'
 
 export interface FourPointChartProps {
@@ -17,6 +21,13 @@ export interface FourPointChartField extends FourPointChartProps {
   _type: 'fourPointChart'
 }
 
+const bgSvgs = [
+  <Circle />,
+  <Hexagon />,
+  <Diamond />,
+  <Triangle />,
+]
+
 export default function FourPointChart({ header, points }: FourPointChartProps) {
   return (
     <div className={styles.wrap}>
@@ -31,7 +42,7 @@ export default function FourPointChart({ header, points }: FourPointChartProps) 
           </div>
           {points && (
             <div className={styles.points}>
-              {points.map((point) => {
+              {points.map((point, index) => {
                 const {
                   _key,
                   title,
@@ -42,6 +53,9 @@ export default function FourPointChart({ header, points }: FourPointChartProps) 
                     <div key={_key} className={styles.point_Inner}>
                       {title && <h3 className={styles.point_Title}>{title}</h3>}
                       {text && <p className={styles.point_Text}>{text}</p>}
+                    </div>
+                    <div className={styles.point_HoverBg}>
+                      {bgSvgs[index]}
                     </div>
                   </div>
                 )
