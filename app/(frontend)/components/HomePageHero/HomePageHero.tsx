fb05bec153929ab07f1960ea626ef1e156b53f8f
Jason Herring <jason@ordinarykids.com>
2023-08-17 11:49:39 -0700

    Option1 - full screen bg

--- app/(frontend)/components/HomePageHero/HomePageHero.tsx
+++ app/(frontend)/components/HomePageHero/HomePageHero.tsx
@@ -1,27 +1,75 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
 import cx from 'classnames'
-import React from 'react'
-import Image from 'next/image'
 import type { PortableTextBlock } from '@portabletext/types'
 import { PortableText } from '@portabletext/react'
-import type ImageField from 'app/(frontend)/types/image'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import type LinkField from 'app/(frontend)/types/link'
+import InteractiveSphere from 'app/(frontend)/components/InteractiveSphere'
 import Button from '../Button/Button'
 import styles from './HomePageHero.module.scss'
 
 export type HomePageHeroType = {
-    header?: string | null,
-    subheader?: PortableTextBlock[],
-    media?: string | null,
-    buttonText?: string | null,
-    buttonLink?: LinkField,
-    image: ImageField,
-  }
+  header?: string | null,
+  subheader?: PortableTextBlock[],
+  buttonText?: string | null,
+  buttonLink?: LinkField,
+}
 
 export default function HomePageHero({
-  header, subheader, media, buttonText, buttonLink, image,
+  header, subheader, buttonText, buttonLink,
 }: HomePageHeroType) {
+  gsap.registerPlugin(ScrollTrigger)
+  // Set up the scope for our gsap context and set our timeline variables
+  const animationRootScope = useRef(null)
+  // don't create timelines inside a ref declaration, create them in the useEffect
+  // https://greensock.com/forums/topic/24383-why-is-my-timeline-working-twice/
+  const scrollTriggerTimeline: React.MutableRefObject<
+    GSAPTimeline | HTMLElement | null
+  > = useRef(null)
+
+  useEffect(() => {
+    // DOM elements ready for animation
+    // Use gsatp matchMedia for responsive targeing
+    const mm = gsap.matchMedia(animationRootScope)
+    // Only run the animation on desktop
+    mm.add(
+      {
+        isDesktop: '(min-width: 1024px)',
+      },
+      (context) => {
+        if (context.conditions) {
+          const { isDesktop } = context.conditions
+          if (isDesktop) {
+            // Create a timeline and add the scrollTrigger for the globe animation
+            scrollTriggerTimeline.current = gsap.timeline({
+              scrollTrigger: {
+                id: 'stID',
+                trigger: animationRootScope.current,
+                start: 'top 80',
+                end: '+=400',
+                pin: true,
+                scrub: 1,
+                pinSpacing: false
+              },
+
+            })
+          }
+        }
+      },
+    )
+
+    return () => {
+      // Cleanup code
+      // Calling revert() on gsap.matchMedia() is the same thing as calling it on gsap.context()
+      mm.revert()
+    }
+  }, [])
+
   return (
-    <div className={cx(styles.wrap)}>
+    <div className={cx(styles.wrap)} ref={animationRootScope}>
       <div className={cx(styles.container)}>
         <div className={cx(styles.heroLeft)}>
           {header && <h1 className={cx(styles.heroHeader)}>{header}</h1>}
@@ -40,24 +88,7 @@ export default function HomePageHero({
           )}
         </div>
         <div className={cx(styles.heroRight)}>
-          {media
-            ? (
-              <div className={cx(styles.heroAnimation)}>
-                <h2>FPO animation</h2>
-              </div>
-            )
-            : (image) && (
-              <div className={cx(styles.heroImage)}>
-                <Image
-                  className={cx(styles.image)}
-                  src={image.url}
-                  width={image.width}
-                  height={image.height}
-                  alt={image.altText ?? ''}
-                  title={image.title ?? undefined}
-                />
-              </div>
-            )}
+          <InteractiveSphere />
         </div>
       </div>
     </div>
---
+++ app/(frontend)/components/InteractiveSphere/InteractiveSphere.tsx
@@ -0,0 +1,130 @@
+// @ts-nocheck
+'use client'
+
+/* eslint-disable */
+import * as THREE from 'three'
+import { Suspense, useEffect, useLayoutEffect, useState, useRef } from 'react'
+import { Canvas, useFrame, useThree, extend } from '@react-three/fiber'
+import { ScrollControls, Sky, OrbitControls,  useScroll, useGLTF, useAnimations } from '@react-three/drei'
+import CameraControls from 'camera-controls'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { gsap } from 'gsap'
+import styles from './InteractiveSphere.module.scss'
+
+extend({ OrbitControls })
+
+const Controls = () => {
+  const { camera, gl } = useThree()
+  const ref = useRef()
+  useFrame(() => ref.current.update())
+  return <OrbitControls enableZoom={false} ref={ref} args={[camera, gl.domElement]} />
+}
+
+
+
+function Sphere() {
+
+  const sphereWrapRef = useRef<HTMLDivElement | null>(null)
+  const [globePosition, setGlobePosition] = useState(0)
+  useEffect(() => {
+    const onScroll = () => {
+      // if (sphereWrapRef.current) {
+      //   const currentScrollY = window.scrollY
+      //   sphereWrapRef.current.style.opacity = `${Math.max(1 - (currentScrollY / 900), 0)}`
+      // }
+      if (window.innerWidth < 800) {
+        setGlobePosition([1, 5, 4])
+      } else {
+        setGlobePosition([-5, -5, 2])
+      }
+    }
+    window.addEventListener('scroll', onScroll)
+    window.addEventListener('resize', onScroll);
+    onScroll()
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+ 
+
+  return (
+    <div id='interactiveSphere' className={styles.wrap} ref={sphereWrapRef}>
+      <Canvas shadows camera={{ position: [1, 1, 44], fov: 300 }} onCreated={((state) => ScrollTrigger.refresh())}>
+        <Controls />
+        <ambientLight intensity={1} />
+        <HelixSphere scale={90} position={globePosition} />
+      </Canvas>
+    </div>
+  )
+}
+ 
+
+function HelixSphere({ ...props }) {
+
+  const sphereWrapRef = useRef<HTMLDivElement | null>(null)
+
+
+  const [currentScrollY, setCurrentScrollY] = useState(0)
+  
+  useEffect(() => {
+    const onScroll = () => {
+      if (sphereWrapRef.current) {
+        setCurrentScrollY(window.scrollY)
+      }
+      // plays animation when page is scrolled.
+      //actions['firstAction'].play().paused = false
+    }
+    window.addEventListener('scroll', onScroll)
+    onScroll();
+    
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-nocheck
+
+  // This hook gives you offets, ranges and other useful things
+  const scroll = useScroll()
+  const { scene, animations } = useGLTF('/w11.glb')
+  const { actions } = useAnimations(animations, scene)
+  //useLayoutEffect(() => Object.values(nodes).forEach((node) => (node.receiveShadow = node.castShadow = true)))
+ // useEffect(() => void (actions['firstAction'].play().paused = true), [actions])
+  // useFrame((state, delta) => { 
+  //   // const action = actions['Take 001']
+  //   // The offset is between 0 and 1, you can apply it to your models any way you like
+  //   const offset = 1 - currentScrollY / 900;
+  //  // console.log(offset)
+  //   // action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * offset, 100, delta)
+  //   state.camera.position.set(10 / offset, 1 * offset*4, 120*offset)
+  //   state.camera.rotation.set(0, offset * 42, offset * 2)
+  //   state.camera.lookAt(0, 0, 0)
+  //   //console.log(state.camera)
+  // })
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.to(sphereWrapRef.current.rotation, 
+        { z: "+=.03", repeat: -1, ease: 'none', repeatRefresh: true })
+      gsap.to(sphereWrapRef.current.rotation,
+        { y: "-=.03", repeat: -1, ease: 'none', repeatRefresh: true })
+      gsap.to(sphereWrapRef.current.rotation,
+        { x: "+=.03", repeat: -1, ease: 'none', repeatRefresh: true })
+    }, sphereWrapRef); // <- scopes all selector text inside the context to this component (optional, default is document)
+    return () => ctx.revert(); // cleanup! 
+
+  }, []);
+
+
+
+  
+  return <primitive object={scene} {...props} ref={sphereWrapRef} />
+}
+
+/*
+author: glenatron (https://sketchfab.com/glenatron)
+license: CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+source: https://sketchfab.com/models/94b24a60dc1b48248de50bf087c0f042
+title: Littlest Tokyo */
+useGLTF.preload('/w11.glb')
+
+
+
+export default Sphere
