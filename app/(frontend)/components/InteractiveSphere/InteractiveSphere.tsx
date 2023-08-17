6e879d8a4ee725f91daee2e4fe16771a1337bddd
Jason Herring <jason@ordinarykids.com>
2023-08-17 14:20:38 -0700

    back to orbit controls

--- app/(frontend)/components/InteractiveSphere/InteractiveSphere.tsx
+++ app/(frontend)/components/InteractiveSphere/InteractiveSphere.tsx
@@ -2,7 +2,6 @@
 
 // @ts-nocheck
 
-
 /* eslint-disable */
 import * as THREE from 'three'
 import { Suspense, useEffect, useLayoutEffect, useState, useRef } from 'react'
@@ -15,12 +14,12 @@ import styles from './InteractiveSphere.module.scss'
 
 extend({ OrbitControls })
 
-// const Controls = () => {
-//   const { camera, gl } = useThree()
-//   const ref = useRef()
-//   useFrame(() => ref.current.update())
-//   return <OrbitControls enableZoom={false} ref={ref} args={[camera, gl.domElement]} target={[0,0,0]} center={[-15, 1, 0]} />
-// }
+const Controls = () => {
+  const { camera, gl } = useThree()
+  const ref = useRef()
+  useFrame(() => ref.current.update())
+  return <OrbitControls enableZoom={false} ref={ref} args={[camera, gl.domElement]} target={[0,0,0]} center={[0, 0, 0]} />
+}
 
 
 
@@ -48,7 +47,7 @@ function Sphere() {
        // sphereWrapRef.current.camera.rotation.set(THREE.MathUtils.degToRad(30), 0, 0);
       }
       if (window.innerWidth < 800) {
-        setGlobePosition([1, 5, 4])
+        setGlobePosition([-1,0, 0])
       } else {
         setGlobePosition([0, 0, 0])
       }
@@ -69,10 +68,10 @@ function Sphere() {
 
   return (
     <div id='interactiveSphere' className={styles.wrap} ref={sphereWrapRef}>
-      <Canvas linear flat rotation={[80, 0, 0]}  camera={{ position: [1, 1, 44], rotation: [30, 40, 0], fov: 50 }} onCreated={((state) => ScrollTrigger.refresh())}>
-        <CameraControls ref={cameraControlRef}   />
+      <Canvas linear flat  camera={{  fov: 20 }} onCreated={((state) => ScrollTrigger.refresh())}>
+        <Controls ref={cameraControlRef}   />
         <ambientLight intensity={3} />
-        <HelixSphere scale={9} position={globePosition}  />
+        <HelixSphere scale={.35} position={globePosition}  />
       </Canvas>
     </div>
   )
@@ -90,7 +89,7 @@ function HelixSphere({ ...props }) {
 
   useFrame(({ camera }, delta) => {
   //  console.log(camera)
-    camera.position.set(10, 1, 12)
+  //  camera.position.set(-1.4, 1, 0)
       // controls.current.target.lerp(target, delta)
 
   })
@@ -120,28 +119,28 @@ function HelixSphere({ ...props }) {
 
   // This hook gives you offets, ranges and other useful things
   const scroll = useScroll()
-  const { scene, animations } = useGLTF('/helix-animation-test4.glb')
+  const { scene, animations } = useGLTF('/wtf-18-darker.glb')
   const { actions } = useAnimations(animations, scene)
   //useLayoutEffect(() => Object.values(nodes).forEach((node) => (node.receiveShadow = node.castShadow = true)))
   // useEffect(() => void (actions['firstAction'].play().paused = true), [actions])
-  // useFrame((state, delta) => { 
+  useFrame((state, delta) => { 
   //   // const action = actions['Take 001']
   //   // The offset is between 0 and 1, you can apply it to your models any way you like
   //   const offset = 1 - currentScrollY / 900;
   //  // console.log(offset)
   //   // action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * offset, 100, delta)
-  //   state.camera.position.set(10 / offset, 1 * offset*4, 120*offset)
-  //   state.camera.rotation.set(0, offset * 42, offset * 2)
-  //   state.camera.lookAt(0, 0, 0)
+    // state.camera.position.set(10 / offset, 1 * offset*4, 120*offset)
+    
+    state.camera.lookAt(-1, 0, 0)
   //   //console.log(state.camera)
-  // })
+  })
 
   useEffect(() => {
     let ctx = gsap.context(() => {
       gsap.to(sphereWrapRef.current.rotation,
         { z: "+=.03", repeat: -1, ease: 'none', repeatRefresh: true })
       gsap.to(sphereWrapRef.current.rotation,
-        { y: "-=.03", repeat: -1, ease: 'none', repeatRefresh: true })
+        { y: "+=.03", repeat: -1, ease: 'none', repeatRefresh: true })
       gsap.to(sphereWrapRef.current.rotation,
         { x: "+=.03", repeat: -1, ease: 'none', repeatRefresh: true })
     }, sphereWrapRef); // <- scopes all selector text inside the context to this component (optional, default is document)
@@ -160,7 +159,7 @@ author: glenatron (https://sketchfab.com/glenatron)
 license: CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
 source: https://sketchfab.com/models/94b24a60dc1b48248de50bf087c0f042
 title: Littlest Tokyo */
-useGLTF.preload('/helix-animation-test4.glb')
+useGLTF.preload('/wtf-18-darker.glb')
 
 
 
